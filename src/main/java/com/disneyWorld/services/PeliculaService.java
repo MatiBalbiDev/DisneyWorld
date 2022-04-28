63ccd97f53089e6f2ad535725b98a9abdaea2cbb
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-04-28 15:19:21 -0300

    Cree el servicio para Pelicula

---
+++ src/main/java/com/disneyWorld/services/PeliculaService.java
@@ -0,0 +1,40 @@
+package com.disneyWorld.services;
+
+import java.util.List;
+
+import com.disneyWorld.models.Pelicula;
+
+public class PeliculaService {
+
+	public List<Pelicula> findByPeliculasAsociadas(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Object getByNombre(String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getByOrden(String orden) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object savePelicula(Pelicula pelicula) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object updatePelicula(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object deletePelicula(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
